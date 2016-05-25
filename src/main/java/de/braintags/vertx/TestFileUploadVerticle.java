6207816fb404ab7ee4da5e8ea4f75fff9814853d
Michael Remme <mremme@braintags.de>
2016-05-25 14:48:08 +0200

    simplification

--- src/main/java/de/braintags/vertx/TestFileUploadVerticle.java
+++ src/main/java/de/braintags/vertx/TestFileUploadVerticle.java
@@ -1,9 +1,5 @@
 package de.braintags.vertx;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import io.vertx.core.AbstractVerticle;
@@ -14,7 +10,6 @@ import io.vertx.core.Handler;
 import io.vertx.core.MultiMap;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
-import io.vertx.core.file.FileSystem;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.http.impl.DebugHttpServerImpl;
@@ -26,10 +21,7 @@ import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.CookieHandler;
 
 /**
- * Autobob<br>
- * <br>
- * Copyright: Copyright (c) 26.11.2015 <br>
- * Company: Braintags GmbH <br>
+ * Reproducer for https://github.com/vert-x3/vertx-web/issues/385
  * 
  * @author mremme
  * 
@@ -38,10 +30,7 @@ import io.vertx.ext.web.handler.CookieHandler;
 public class TestFileUploadVerticle extends AbstractVerticle {
   private static final io.vertx.core.logging.Logger LOGGER = io.vertx.core.logging.LoggerFactory
       .getLogger(TestFileUploadVerticle.class);
-  public static final String MOVE_MESSAGE = "moved uploaded file from %s to %s";
   private Router router;
-  private String uploadDirectory = "webroot/upload/";
-  private String uploadRelativePath = "upload/";
   public static final int PORT = 8081;
 
   /*
@@ -76,6 +65,19 @@ public class TestFileUploadVerticle extends AbstractVerticle {
     }
   }
 
+  private void initHttpServer(Router router, Handler<AsyncResult<Void>> handler) {
+    HttpServerOptions options = new HttpServerOptions().setPort(PORT);
+    HttpServer server = new DebugHttpServerImpl((VertxInternal) vertx, options);
+
+    server.requestHandler(router::accept).listen(result -> {
+      if (result.failed()) {
+        handler.handle(Future.failedFuture(result.cause()));
+      } else {
+        handler.handle(Future.succeededFuture());
+      }
+    });
+  }
+
   private void addRouter(Router router) {
     router.route().handler(BodyHandler.create());
     router.route().handler(CookieHandler.create());
@@ -87,8 +89,6 @@ public class TestFileUploadVerticle extends AbstractVerticle {
           context.response().setStatusCode(400);
           context.response().end(getReply(context, "error"));
         } else {
-          Map<String, String> params = extractProperties(entity, context);
-          handleFileUploads(entity, context, params);
           context.response().end(getReply(context, "OK"));
         }
       } catch (Exception e) {
@@ -122,136 +122,6 @@ public class TestFileUploadVerticle extends AbstractVerticle {
         + " | UPLOADED: " + fu.uploadedFileName() + " | SIZE: " + fu.size()).appendString("\n"));
 
     return buffer.appendString("USER: " + context.user());
-  }
-
-  private void handleFileUploads(String entityName, RoutingContext context, Map<String, String> params) {
-    String startKey = entityName.toLowerCase() + ".";
-    Set<FileUpload> fileUploads = context.fileUploads();
-    if (fileUploads.size() == 0) {
-      throw new IllegalArgumentException("No uploads found");
-    }
-    FileSystem fs = getVertx().fileSystem();
-    LOGGER.info("Number of fileuploads: " + fileUploads.size());
-
-    for (FileUpload upload : fileUploads) {
-      if (isHandleUpload(upload, startKey)) {
-        String fieldName = upload.name().toLowerCase();
-        LOGGER.info("uploaded file detected for field name " + fieldName + ", fileName: " + upload.fileName());
-        String relativePath = handleOneFile(fs, upload);
-        String pureKey = fieldName.substring(startKey.length());
-        params.put(pureKey, relativePath);
-      }
-    }
-  }
-
-  private String handleOneFile(FileSystem fs, FileUpload upload) {
-    String uploadedFile = upload.uploadedFileName();
-    String[] newDestination = examineNewDestination(fs, upload);
-    fs.moveBlocking(uploadedFile, newDestination[0]);
-
-    LOGGER.info(String.format(MOVE_MESSAGE, uploadedFile, newDestination[0]));
-    return newDestination[1];
-  }
-
-  private boolean isHandleUpload(FileUpload upload, String startKey) {
-    LOGGER.info(
-        "CHECKING: " + upload.uploadedFileName() + " | fileName: " + upload.fileName() + " | name: " + upload.name());
-    String fieldName = upload.name().toLowerCase();
-    if (upload.size() <= 0) {
-      throw new IllegalArgumentException("In this scenario upload size must be > 0 )");
-    }
-    if (!fieldName.startsWith(startKey)) {
-      LOGGER.info("NOT HANDLED: fieldname does not start with:" + startKey + " | " + fieldName);
-      return false;
-    }
-    return true;
-  }
-
-  private String[] examineNewDestination(FileSystem fs, FileUpload upload) {
-    if (upload.fileName() == null || upload.fileName().hashCode() == 0) {
-      throw new IllegalArgumentException("The upload contains no filename");
-    }
-    String[] destinations = new String[2];
-    String upDir = uploadDirectory;
-    if (!fs.existsBlocking(upDir)) {
-      fs.mkdirsBlocking(upDir);
-    }
-    String relDir = uploadRelativePath;
-    String fileName = createUniqueName(fs, upDir, upload.fileName());
-    destinations[0] = upDir + (upDir.endsWith("/") ? "" : "/") + fileName;
-    destinations[1] = relDir + (relDir.endsWith("/") ? "" : "/") + fileName;
-    return destinations;
-  }
-
-  private String createUniqueName(FileSystem fs, String upDir, String fileName) {
-    fileName = fileName.replaceAll(" ", "_");
-    String newFileName = fileName;
-    int counter = 0;
-    String path = upDir + (upDir.endsWith("/") ? "" : "/") + newFileName;
-    while (fs.existsBlocking(path)) {
-      LOGGER.info("file exists already: " + path);
-      if (fileName.indexOf('.') > 0) {
-        newFileName = fileName.replaceFirst("\\.", counter++ + ".");
-      } else {
-        newFileName = fileName + counter++;
-      }
-      path = upDir + (upDir.endsWith("/") ? "" : "/") + newFileName;
-    }
-    return newFileName;
-  }
-
-  /**
-   * Extract the properties from the request, where the name starts with the entity name, which shall be handled by the
-   * current request
-   * 
-   * @param entityName
-   *          the name, like it was specified by the parameter {@link PersistenceController#MAPPER_CAPTURE_KEY}
-   * @param captureMap
-   *          the resolved capture parameters for the current request
-   * @param context
-   *          the {@link RoutingContext} of the request
-   * @param mapper
-   *          the IMapper for the current request
-   * @return the key / values of the request, where the key starts with "entityName.". The key is reduced to the pure
-   *         name
-   */
-  protected Map<String, String> extractProperties(String entityName, RoutingContext context) {
-    String startKey = entityName.toLowerCase() + ".";
-    Map<String, String> map = new HashMap<>();
-    extractPropertiesFromMap(startKey, map, context.request().formAttributes());
-    extractPropertiesFromMap(startKey, map, context.request().params());
-    return map;
-  }
-
-  /**
-   * @param startKey
-   * @param map
-   * @param attrs
-   */
-  private void extractPropertiesFromMap(String startKey, Map<String, String> map, MultiMap attrs) {
-    Iterator<Entry<String, String>> it = attrs.iterator();
-    while (it.hasNext()) {
-      Entry<String, String> entry = it.next();
-      String key = entry.getKey().toLowerCase();
-      if (key.startsWith(startKey)) {
-        String pureKey = key.substring(startKey.length());
-        String value = entry.getValue();
-        map.put(pureKey, value);
-      }
-    }
-  }
-
-  private void initHttpServer(Router router, Handler<AsyncResult<Void>> handler) {
-    HttpServerOptions options = new HttpServerOptions().setPort(PORT);
-    HttpServer server = new DebugHttpServerImpl((VertxInternal) vertx, options);
-
-    server.requestHandler(router::accept).listen(result -> {
-      if (result.failed()) {
-        handler.handle(Future.failedFuture(result.cause()));
-      } else {
-        handler.handle(Future.succeededFuture());
-      }
-    });
   }
 
   private void logRequest(RoutingContext context) {
--- src/test/java/de/braintags/vertx/test/ExecuteRequests.java
+++ src/test/java/de/braintags/vertx/test/ExecuteRequests.java
@@ -26,9 +26,7 @@ public class ExecuteRequests {
   private static final io.vertx.core.logging.Logger LOGGER = io.vertx.core.logging.LoggerFactory
       .getLogger(ExecuteRequests.class);
   private String HOSTNAME = "localhost";
-  // private String HOSTNAME = "192.168.41.69";
   // private String HOSTNAME = "78.138.125.69";
-  // private String HOSTNAME = "serverscope.de";
   private int PORT = 8081;
 
   protected static Vertx vertx;
